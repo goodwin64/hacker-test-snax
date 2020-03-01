d3ab888df495538d822ac20ad3e98abc0b665bf2
Max Donchenko <maxx.donchenko@gmail.com>
2020-03-01 22:32:31 +0200

    Updates

---
+++ precache-manifest.4a79d6f5065a4697331a7f8c9f742c41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecfd84ae23747cc411e5bbacec8ba51c",
+    "url": "/hacker-test-snax/index.html"
+  },
+  {
+    "revision": "7376227814903cf0aafa",
+    "url": "/hacker-test-snax/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ffab44789f19bcda3061",
+    "url": "/hacker-test-snax/static/js/2.43065643.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/hacker-test-snax/static/js/2.43065643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7376227814903cf0aafa",
+    "url": "/hacker-test-snax/static/js/main.dd474443.chunk.js"
+  },
+  {
+    "revision": "e012a07f4937e0fb143f",
+    "url": "/hacker-test-snax/static/js/runtime-main.9e74d759.js"
+  }
+]);
